2faf88acba83194c6527871d25c3be548d84b59e
tinhdev <iamphamhoangtinh@gmail.com>
2022-05-02 12:37:57 +0700

    trello-02 create class account and class news

---
+++ src/app/controllers/account/AccountController.js
@@ -0,0 +1,8 @@
+const Course = require('../../models/Course');
+const { mongooseToObject } = require('../../../until/mongoose');
+
+class AccountController {
+
+}
+
+module.exports = new AccountController();
---
+++ src/app/controllers/news/NewsController.js
@@ -0,0 +1,8 @@
+const Course = require('../../models/Course');
+const { mongooseToObject } = require('../../../until/mongoose');
+
+class NewsController {
+
+}
+
+module.exports = new NewsController();
