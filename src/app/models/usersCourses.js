da9ab92e3271767a951a1639bc0dca1589f39f4b
thanhquyendev <quyen.ngo@pizzahut.io>
2022-05-04 22:09:43 +0700

    https://trello.com/c/9ZEyhJVP/10-api-post-course

--- src/app/models/usersCourses.js
+++ src/app/models/usersCourses.js
@@ -3,3 +3,32 @@ const slug = require('mongoose-slug-generator');
 const mongooseDelete = require('mongoose-delete');
 
 const Schema = mongoose.Schema;
+
+
+let dataDefaul = {
+    _id: 12,
+    user_id: 1,
+    courses: {
+        course_detail: [
+        {
+            course_id: 12,
+            nameCourse: "NestJS",
+            description: "How to learn about NestJS?",
+            image: "image/nestjs",
+            videoId: 'ieqw222',
+            level: 'advanced',
+            slug: "nestjs-1",
+        },
+        {
+            course_id: 13,
+            nameCourse: "NestJS",
+            description: "How to learn about NestJS?",
+            image: "image/nestjs",
+            videoId: 'ieqw222',
+            level: 'advanced',
+            slug: "nestjs-1",
+        }
+     ]
+    },
+    active: true,
+}
