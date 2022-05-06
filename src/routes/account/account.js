d6db5b1057f3ec7e51b30efae1a36a64c0f362da
Quyen Ngo <quyenngo@ip-172-16-3-101.ap-southeast-1.compute.internal>
2022-05-06 16:34:03 +0700

    create services api account

---
+++ src/routes/account/account.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const accountController = require('../../app/controllers/account/AccountController');
+
+
+
+
+module.exports = router;
